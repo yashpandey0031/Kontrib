12fcd5792dc9c956e885837e278781fb7dbe1836
Yash Pandey <yashpandey1556@gmail.com>
2026-06-08 14:58:07 +0530

    added the core features

--- backend/routers/analysis.py
+++ backend/routers/analysis.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from github_client import get_user,get_top_contributors,get_user_commits
-from analyzer import analyze_commits, compare
+from analyzer import analyze_commits, compare, extract_commit_messages
+from llm_client import analyze_with_llm
 
 #pydantic fetches the incoming request and automatically validates incoming JSON 
 
@@ -41,10 +42,26 @@ async def analyze(request: AnalysisRequest):
   #compare
   comparison = compare(user_metrics, benchmark_metrics)
 
+  your_messages = extract_commit_messages(user_commits)
+  benchmark_messages = extract_commit_messages(benchmark_commits)
+
+  result = analyze_with_llm(
+    username=request.username,
+    user_messages=your_messages,
+    benchmark_username=top[0]['login'],
+    benchmark_messages=benchmark_messages,
+    metrics_comparison=comparison
+    )   
+
+
+
+
+
   return {
     "user" : request.username,
     "user_repo" : request.user_repo,
     "benchmark_repo" : request.benchmark_repo,
     "benchmark_contributor" : top[0]['login'],
-    "analysis" : comparison
+    "analysis" : comparison,
+    "result" : result,
   }
